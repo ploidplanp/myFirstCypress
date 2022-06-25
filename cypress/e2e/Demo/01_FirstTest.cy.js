bb3c8631563eddf176087267ffd07e75d5bac489
ploidplanp <waoekpr@gmail.com>
2022-06-26 00:05:57 +0700

    First Commit

---
+++ cypress/e2e/Demo/01_FirstTest.cy.js
@@ -0,0 +1,8 @@
+// Test Suite
+describe('My First Test Suite', function () {
+    // Test Case
+    it('Does not do much!', function() {
+        const sum = 1 + 1
+        expect(3).to.equal(sum)
+    })
+})
